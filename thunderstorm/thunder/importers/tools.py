02b02037f20cc468905b5adf46cf49f697ccf7ae
David Trémouilles <david.trem@gmail.com>
2013-08-13 16:22:14 +0200

    Update and clean-up licence information

--- thunderstorm/thunder/importers/tools.py
+++ thunderstorm/thunder/importers/tools.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2010 Trémouilles David
+#  Copyright (c) 2013 ESDAnalysisTools Development Team
+#  Copyright (C) 2010 Trémouilles David
 
-#This file is part of Thunderstorm.
+#  This file is part of Thunderstorm.
 #
-#ThunderStrom is free software: you can redistribute it and/or modify
-#it under the terms of the GNU Lesser General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+#  ThunderStrom is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#ThunderStorm is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU Lesser General Public License for more details.
+#  ThunderStorm is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
 #
-#You should have received a copy of the GNU Lesser General Public License
-#along with ThunderStorm.  If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with ThunderStorm.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Define the plugin class mother of all plugins.
